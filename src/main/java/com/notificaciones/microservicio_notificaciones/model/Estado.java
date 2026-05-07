35cd669162b8bdc48e9458eb51b69b053ef314e2
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-05-07 18:47:39 -0400

    Actualización de modelo a entidad JPA y actualización de reglas de negocio

--- src/main/java/com/notificaciones/microservicio_notificaciones/model/Estado.java
+++ src/main/java/com/notificaciones/microservicio_notificaciones/model/Estado.java
@@ -1,33 +1,24 @@
 package com.notificaciones.microservicio_notificaciones.model;
 
 import jakarta.persistence.*;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Entity
-@Table(name = "notificacion")
+@Table
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Notificacion {
+public class Estado {
 
-    @Column(nullable = false, length = 5)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idNotificacion;
+    @Column(nullable = false, length = 3)
+    private Long idEstado;
 
     @Column(nullable = false, length = 50)
-    private String tipo;
-
-    @Column(nullable = false)
-    private String mensaje;
-
-    @Column(nullable = false)
-    private LocalDate fecha_envio;
+    private String estado;
 
 
 }
