72d0a4a2b71a19f34f418ffe06d9d3f4978d7181
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-05-11 12:52:47 -0400

    Configuración de FeignClient e implementación de DTOs para conexión

---
+++ src/main/java/com/notificaciones/microservicio_notificaciones/dto/AuditoriaResponseDTO.java
@@ -0,0 +1,19 @@
+package com.notificaciones.microservicio_notificaciones.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuditoriaResponseDTO {
+
+    private Long idAuditoria;
+    private String detalle;
+    private LocalDate fecha;
+
+
+
+}
